9038fa0b710cd328d37ddd57428195cb700e8fee
Argwings Shikuku <siargwi@gmail.com>
2025-10-09 20:35:31 +0100

    project 2

---
+++ SEIR.R
@@ -0,0 +1,17 @@
+set.seed(0)
+n<-sample(5,1000,replace = T);rep(1000)
+n
+get.net(beta,h,nc=15){
+  beta=n
+  bat<-("")
+  for (i in 1:length(beta)) {
+    if (i!=i){
+     result[i]<-beta 
+    }
+  
+  }return(result_matrix)
+}
+
+get.net(50, 3, nc = 15)
+
+
