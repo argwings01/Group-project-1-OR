65aaf140f33beac2838aef824a6c4bd5744358f7
argwings01 <siargwi@gmail.com>
2025-10-10 20:26:31 +0100

    here

--- Assignment2.R
+++ Assignment2.R
@@ -1,39 +1,22 @@
 set.seed(0)
-h_max<-5
-
-h<-rep(0,1000)
-for (i in 1:1000) {
-  m<-sample(1:h_max)[[1]]  #size of each household
-  h[i]<-rep(rep("i",sample(h_max)[1]),1000)
-  }
-h<-sample(1:h_max, rep(sample(n)))
-
-set.seed(0)
-n<- rep(1:200,5); sample(n, 1000, replace = T)
-
-n
-
-
-# 1. Create a sequence of unique numbers (e.g., from 1 to 200).
-unique_values <- 1:200
-
-# 2. Repeat each unique number 5 times. This results in a vector of length 1000.
-repeated_values <- rep(unique_values, each = 5)
-
-# 3. Randomly shuffle the order of the 1000 numbers.
-random_numbers <- sample(repeated_values, size = 1000, replace = FALSE)
-
-
-
-n<-sample(5,1000,replace = T);rep(1000)
-n
+n<-1000
+h_max=5
+h <- sample(rep(1:n,sample(1:h_max,n,replace = T))[1:n])
+h
 get.net<-function(beta,h,nc=15){
-  beta=n
-  bat<-("")
-  for (i in 1:length(beta)) {
-    if (i!=i){
-      result[i]<-beta 
-    }
+  av_beta<-mean(beta)
+  beta2<-beta
+  constant<-nc/((av_beta^2) *(n-1))
+  for (i in n:2) {
+    temp<-beta2[-1]
+    beta2[i+1]<-beta2[i]
+    beta2[1]<-temp
+  }
+    
+#    for (j in 1:length(n)) {
+ #   if (h[i]==h[j]){
+  # beta_j
+    
     
   }return(result_matrix)
 }
